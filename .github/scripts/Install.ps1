d7bfa5385d63cc1263563b1599e207a7ead2fc2e
Michael Kelley <michael.a.kelley@gmail.com>
2019-09-07 19:12:17 -0700

    Revert env var access

--- .github/scripts/Install.ps1
+++ .github/scripts/Install.ps1
@@ -1,14 +1,12 @@
 $ErrorActionPreference = "Stop"
 
-gci env: 
-
 # Force bootstrap of the Nuget PackageManagement Provider; Reference: http://www.powershellgallery.com/GettingStarted?section=Get%20Started 
 Get-PackageProvider -Name NuGet -Force -Verbose
 
 # get fzf:
 if ($isLinux) {
   $modules = 'Pester', 'platyPS'
-  Invoke-WebRequest https://github.com/junegunn/fzf-bin/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tgz -OutFile fzf.tgz -Verbose
+  Invoke-WebRequest https://github.com/junegunn/fzf-bin/releases/download/${env:FZF_VERSION}/fzf-${env:FZF_VERSION}-linux_amd64.tgz -OutFile fzf.tgz -Verbose
   mkdir ./fzf/
   tar -xvf ./fzf.tgz -C ./fzf/
 } else {
@@ -16,7 +14,7 @@ if ($isLinux) {
   # choco install pester
   
   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-  Invoke-WebRequest https://github.com/junegunn/fzf-bin/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-windows_amd64.zip -OutFile fzf.zip -Verbose
+  Invoke-WebRequest https://github.com/junegunn/fzf-bin/releases/download/${env:FZF_VERSION}/fzf-${env:FZF_VERSION}-windows_amd64.zip -OutFile fzf.zip -Verbose
   Expand-Archive fzf.zip
 }
 
