ff9ab7590e0bbf2e5717456aeff2055106a60fa0
Michael Kelley <michael.a.kelley@gmail.com>
2019-09-27 07:37:51 -0700

    Properly set PATH env var for Mac

--- .github/scripts/Tests.ps1
+++ .github/scripts/Tests.ps1
@@ -1,11 +1,8 @@
 $ErrorActionPreference = "Stop"
 
 # make sure fzf is in our path:
-if ($isLinux) {
-  $env:PATH += ":./fzf"
-} else {
-  $env:PATH += ";./fzf"
-}
+$env:PATH = '{0}{1}{2}' -f $env:PATH,[IO.Path]::PathSeparator,"./fzf"
+
 $testResultsFile = "./TestsResults.xml"
 $res = Invoke-Pester -Script ./PSFzf.tests.ps1 -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
 #(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
