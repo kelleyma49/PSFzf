b7d1de62978c42324ce505356de7495691694b19
Michael Kelley <michael.a.kelley@gmail.com>
2019-12-03 20:13:19 -0800

    WIP

---
+++ PSFzf-Binary/Helpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+
+namespace PSFzf
+{
+    public static class Helpers
+    {
+        static public IEnumerable<string> CompleteInput(string input, int cursorIndex, Hashtable options)
+        {
+            System.Management.Automation.PowerShell ps;
+            ps = System.Management.Automation.PowerShell.Create(RunspaceMode.CurrentRunspace);
+            
+            var completion = CommandCompletion.CompleteInput(input, cursorIndex, options, ps);
+            var matches = completion?.CompletionMatches;
+            if (matches?.Count>0 && (matches[0]?.ResultType == CompletionResultType.ProviderItem || matches[0]?.ResultType == CompletionResultType.ProviderContainer))
+            {
+                yield return "blah";
+            }
+            else if (matches != null)
+            {
+                foreach (var item in matches)
+                {
+                    yield return item.CompletionText;
+                }
+            }
+        }
+
+    }
+}
