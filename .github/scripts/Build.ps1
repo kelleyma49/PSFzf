51000f444e654c8b6dddeede2ca9b3b542443fae
Michael Kelley <michael.a.kelley@gmail.com>
2020-01-27 22:50:48 -0800

    Resolves issue #32
    - add support for the following cmd line options:
      -- phony
      -- filepath-word
      -- layout
      -- border
      -- info
      -- tabstop
      -- no-bold
      -- expect
    - requires fzf version >= 0.20.0
    - fix for github action build script

--- .github/scripts/Build.ps1
+++ .github/scripts/Build.ps1
@@ -1,3 +1,6 @@
 $env:DOTNET_CLI_TELEMETRY_OPTOUT=1
 dotnet build --configuration Release --output . .\PSFzf-Binary\PSFzf-Binary.csproj
-copy-item .\PSFzf-Binary\PSFzf.dll . -verbose
+if (-not (Test-path .\PSFzf.dll -PathType Leaf)) {
+    Get-ChildItem . -Recurse 
+    throw "Unable to find PSFzf.dll"
+}
