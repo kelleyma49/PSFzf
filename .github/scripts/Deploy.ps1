5edbe7c9f83ce2df4f16f04f28db634b9051dc08
Michael Kelley <michael.a.kelley@gmail.com>
2020-02-29 13:40:10 -0800

    Move Publish step to pwsh
    - pwsh should have -Prerelease argument for Publish-Module
    - Update Deploy.ps1 script to be platform agnostic

--- .github/scripts/Deploy.ps1
+++ .github/scripts/Deploy.ps1
@@ -2,21 +2,21 @@ $ErrorActionPreference = "Stop"
 Write-Host "Triggered from ${env:GITHUB_REF}"
     
 $psdir = $env:GITHUB_WORKSPACE
-$installdir = join-path $psdir 'PSFzf'
+$installdir = Join-Path $psdir 'PSFzf'
 new-item $installdir -ItemType Directory -verbose
-copy-item $psdir\*.ps* $installdir -verbose
-copy-item $psdir\PSFzf.dll $installdir -verbose
+copy-item $(Join-Path $psdir '*.ps*') $installdir -verbose
+copy-item $(Join-Path $psdir 'PSFzf.dll') $installdir -verbose
 
 # generate documentation:
 $docdir = Join-Path $installdir 'en-US'
 new-item $docdir -ItemType Directory
 Import-Module platyPS
 platyPS\New-ExternalHelp (Join-Path $psdir 'docs') -Force -OutputPath $docdir
-copy-item $psdir\en-US\*.txt $docdir -verbose
+copy-item $(Join-Path $psdir 'en-US' '*.txt') $docdir -verbose
 
 # get contents of current psd, update version and save it back out in the publish directory:
 $psdFilePath = Join-Path $installdir 'PSFzf.psd1'
-$psdTable = Invoke-Expression (Get-Content $psdFilePath  | out-string) 
+$psdTable = Invoke-Expression (Get-Content $psdFilePath  | Out-String) 
 $version = $env:GITHUB_REF
 if ($version -eq '' -or $null -eq $version) {
   throw 'Version not found in $GITHUB_REF'
