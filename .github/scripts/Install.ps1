c1b8e1103854561a77012a0b213652ffdf7e6ab4
Michael Kelley <michael.a.kelley@gmail.com>
2020-03-12 00:08:37 -0700

    Install Pester on Windows
    - Gets latest version of Pester for CI machines

--- .github/scripts/Install.ps1
+++ .github/scripts/Install.ps1
@@ -2,17 +2,14 @@ $ErrorActionPreference = "Stop"
 
 # Force bootstrap of the Nuget PackageManagement Provider; Reference: http://www.powershellgallery.com/GettingStarted?section=Get%20Started 
 Get-PackageProvider -Name NuGet -Force -Verbose
+$modules = 'Pester', 'platyPS'
 
 # get fzf:
 if ($IsLinux -or $IsMacOS) {
-  $modules = 'Pester', 'platyPS'
   Invoke-WebRequest https://github.com/junegunn/fzf-bin/releases/download/${env:FZF_VERSION}/fzf-${env:FZF_VERSION}-linux_amd64.tgz -OutFile fzf.tgz -Verbose
   mkdir ./fzf/
   tar -xvf ./fzf.tgz -C ./fzf/
-} else {
-  $modules = 'platyPS'
-  # choco install pester
-  
+} else {  
   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
   Invoke-WebRequest https://github.com/junegunn/fzf-bin/releases/download/${env:FZF_VERSION}/fzf-${env:FZF_VERSION}-windows_amd64.zip -OutFile fzf.zip -Verbose
   Expand-Archive fzf.zip
