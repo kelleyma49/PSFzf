357e4b8ad74f506c49b859e1d8b329cf9eb1e7df
Michael Kelley <michael.a.kelley@gmail.com>
2020-12-27 15:06:52 -0800

    Preview enhancements
    - only run Get-Help on non-applications
    - show git log on directory preview

--- helpers/PsFzfTabExpansion-Preview.ps1
+++ helpers/PsFzfTabExpansion-Preview.ps1
@@ -1,6 +1,10 @@
 [CmdletBinding()]
 param ($DirName,$Item)
 
+$RunningInWindowsTerminal = [bool]($env:WT_Session)
+$IsWindowsCheck = ($PSVersionTable.PSVersion.Major -le 5) -or $IsWindows
+$ansiCompatible = $script:RunningInWindowsTerminal -or (-not $script:IsWindowsCheck)
+
 if ([System.IO.Path]::IsPathRooted($Item)) {
     $path = $Item
 } else {
@@ -8,22 +12,35 @@ if ([System.IO.Path]::IsPathRooted($Item)) {
 }
 # is directory?
 if (Test-Path $path -PathType Container) {
+    
+    # don't output anything if not a git repo
+    Push-Location $path 
+    if ($ansiCompatible) {
+        git log --color=always -1 2> $null
+    } else {
+        git log -1 2> $null
+    }
+    Pop-Location
+
     Get-ChildItem $path
 } 
 # is file?
 elseif (Test-Path $path -PathType leaf) {
-    $RunningInWindowsTerminal = [bool]($env:WT_Session)
-    $IsWindowsCheck = ($PSVersionTable.PSVersion.Major -le 5) -or $IsWindows
-
     # use bat (https://github.com/sharkdp/bat) if it's available:
-    $checkBat = $script:RunningInWindowsTerminal -or (-not $script:IsWindowsCheck)
-    if ($checkBat -and $(Get-Command bat -ErrorAction SilentlyContinue)) {
+    if ($ansiCompatible -and $(Get-Command bat -ErrorAction SilentlyContinue)) {
         bat --style=numbers,changes --color always $path
     } else {
         Get-Content $path
     }
 }
-# PowerShell command 
-elseif (Get-Command $Item -ErrorAction SilentlyContinue) {
-    Get-Help $Item
+# PowerShell command?
+elseif (($cmdResults = Get-Command $Item -ErrorAction SilentlyContinue)) {
+    if ($cmdResults) {
+        if ($cmdResults.CommandType -ne 'Application') {
+            Get-Help $Item
+        } else {
+            # just output application location:
+            $cmdResults.Source
+        } 
+    }
 }
