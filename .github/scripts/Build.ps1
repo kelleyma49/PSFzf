7f96477fd45f7d807ee9cb1b1b973070c397e05b
Michael Kelley <michael.a.kelley@gmail.com>
2021-04-29 21:30:40 -0700

    Keep trying

--- .github/scripts/Build.ps1
+++ .github/scripts/Build.ps1
@@ -1,7 +1,8 @@
 $ErrorActionPreference = "Stop"
 
 $env:DOTNET_CLI_TELEMETRY_OPTOUT=1
-dotnet restore --verbosity detailed
+#dotnet restore --verbosity detailed
+dotnet add package PowerShellStandard.Library --version 5.1.0
 dotnet build --configuration Release PSFzf.sln
 $dllPaths = Get-ChildItem PSFzf.dll -Recurse
 if ($null -eq $dllPaths) {
