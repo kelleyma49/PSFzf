a88dae0d543223e87fa1d02b278f183f80b3551c
Michael Kelley <michael.a.kelley@gmail.com>
2022-05-29 10:54:53 -0700

    Upgrade to Pester 5

--- .github/scripts/Install.ps1
+++ .github/scripts/Install.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-# Force bootstrap of the Nuget PackageManagement Provider; Reference: http://www.powershellgallery.com/GettingStarted?section=Get%20Started 
+# Force bootstrap of the Nuget PackageManagement Provider; Reference: http://www.powershellgallery.com/GettingStarted?section=Get%20Started
 Get-PackageProvider -Name NuGet -Force -Verbose
 
 # get fzf:
@@ -13,20 +13,20 @@ if ($IsLinux -or $IsMacOS) {
   Invoke-WebRequest https://github.com/junegunn/fzf/releases/download/${env:FZF_VERSION}/fzf-${env:FZF_VERSION}-${prefix}_amd64.tar.gz -OutFile fzf.tgz -Verbose
   mkdir ./fzf/
   tar -xvf ./fzf.tgz -C ./fzf/
-} else {  
+} else {
   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
   Invoke-WebRequest https://github.com/junegunn/fzf/releases/download/${env:FZF_VERSION}/fzf-${env:FZF_VERSION}-windows_amd64.zip -OutFile fzf.zip -Verbose
   Expand-Archive fzf.zip
 }
 
-$modules = @('Pester', '4.10.1'), @('platyPS', $null), @('PSScriptAnalyzer', $null)
-$modules | ForEach-Object { 
+$modules = @('Pester', $null), @('platyPS', $null), @('PSScriptAnalyzer', $null)
+$modules | ForEach-Object {
   $module = $_[0]
   $version = $_[1]
   if ($null -ne $version) {
     Install-Module -Name $module -Scope CurrentUser -RequiredVersion $version -Force -Verbose
   } else {
-    Install-Module -Name $module -Scope CurrentUser -Force -Verbose 
+    Install-Module -Name $module -Scope CurrentUser -Force -Verbose
   }
   Import-Module -Name $module -Verbose
 }
