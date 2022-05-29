kelleyma49/PSFzf

commit e14659683ed2c646f2e6172e47ddef8780b63d9c

Resolve #85
- don't use PrintQuery as I'm getting unexpected output ordering under
  Powershell 5

PSFzf.TabExpansion.ps1
@@ -272,30 +272,27 @@ function script:Invoke-FzfTabCompletionInner()
             -Layout reverse `
             -Expect $expectTrigger `
             -Query "$prefix" `
-            -PrintQuery `
             -Bind 'tab:down,btab:up' `
             @additionalCmd | ForEach-Object {
-                if ($null -eq $_) {
-                    $script:fzfOutput += ""
-                } else {
+                if ($null -ne $_ -and -not [string]::IsNullOrWhiteSpace($_)) {
                     $script:fzfOutput += $_
                 }
             }
-
+    
         # check if there's a selection:
-        if ($script:fzfOutput.Length -gt 2) {
-            $script:result = $script:fzfOutput[2..($script:fzfOutput.Length-1)]
+        if ($script:fzfOutput.Length -ge 1) {
+            $script:result = $script:fzfOutput[0]
         }
         # or just complete with the query string: 
         else {
-            $script:result = $script:fzfOutput[0]
+            $script:result = $prefix
         }    
 
         # check if we should continue completion:
         $script:continueCompletion = $script:fzfOutput[1] -eq $script:TabContinuousTrigger
     
 		InvokePromptHack
-	}	
+    }	
 
 	$result = $script:result
 	if ($null -ne $result) {

commit 91580f99551439c4c76d80e4f81778c35a875300

Fix handling of continuous trigger
- I broke this in #119

PSFzf.TabExpansion.ps1
@@ -283,14 +283,12 @@ function script:Invoke-FzfTabCompletionInner()
             -Query "$prefix" `
             -Bind 'tab:down,btab:up' `
             @additionalCmd | ForEach-Object {
-                if ($null -ne $_ -and -not [string]::IsNullOrWhiteSpace($_)) {
-                    $script:fzfOutput += $_
-                }
+                $script:fzfOutput += $_
             }
 
         # check if there's a selection:
-        if ($script:fzfOutput.Length -ge 1) {
-            $script:result = $script:fzfOutput[0]
+        if ($script:fzfOutput.Length -gt 1) {
+            $script:result = $script:fzfOutput[1]
         }
         # or just complete with the query string:
         else {
@@ -298,7 +296,7 @@ function script:Invoke-FzfTabCompletionInner()
         }
 
         # check if we should continue completion:
-        $script:continueCompletion = $script:fzfOutput[1] -eq $script:TabContinuousTrigger
+        $script:continueCompletion = $script:fzfOutput[0] -eq $script:TabContinuousTrigger
 
 		InvokePromptHack
     }