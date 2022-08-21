f4a9d9ee3e0125eff198c129486d044b810c5228
Michael Kelley <michael.a.kelley@gmail.com>
2022-08-21 13:57:15 -0700

    Fix line endings (#179)

--- helpers/PsFzfGitHashes-Preview.sh
+++ helpers/PsFzfGitHashes-Preview.sh
@@ -5,5 +5,5 @@ hash=$1
 
 pushd . > /dev/null
 cd "$dir"
-echo $hash | grep -o "[a-f0-9]\{7,\}" | xargs git show $args 
+echo $hash | grep -o "[a-f0-9]\{7,\}" | xargs git show $args
 popd > /dev/null
