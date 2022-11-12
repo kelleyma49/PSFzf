0364b14bbd5013a37348fe6681c4980195515ab2
Michael Kelley <michael.a.kelley@gmail.com>
2022-11-12 11:56:22 -0800

    Resolves Issue 196 (#199)
    * Issue_196 > editor is launched in background with ie
    
    * Add Wait parameter
    
    - Add Wait parameter in case old behavior is desired
    
    Co-authored-by: belotn <belot.nicolas@gmail.com>

--- PSFzf.Functions.ps1
+++ PSFzf.Functions.ps1
@@ -88,7 +88,7 @@ function Get-EditorLaunch() {
     }
 }
 function Invoke-FuzzyEdit() {
-    param($Directory = ".")
+    param($Directory = ".", [switch]$Wait)
 
     $files = @()
     try {
@@ -123,7 +123,8 @@ function Invoke-FuzzyEdit() {
             # Not sure if being passed relative or absolute path
             $cmd = Get-EditorLaunch -FileList $files
             Write-Host "Executing '$cmd'..."
-            Invoke-Expression -Command $cmd
+            ($Editor, $Arguments) = $cmd.Split(' ')
+            Start-Process $Editor -ArgumentList $Arguments -Wait:$Wait -NoNewWindow
         }
         catch {
         }
