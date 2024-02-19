b6b7b3f9ff84de6f201dc5de84b00f50d7415141
Michael Kelley <michael.a.kelley@gmail.com>
2024-02-18 21:13:41 -0800

    Add check viewing support
    - Works for GitHub and ADO
    - Checks can be rerun in ADO
    - PR selection initially starts with current user, but is selectable with a keyboard shortcut to list all users

--- PSFzf.Git.ps1
+++ PSFzf.Git.ps1
@@ -268,74 +268,219 @@ function Invoke-PsFzfGitPullRequests() {
         Write-Error "git executable could not be found"
         return
     }
-    # find the repo remote URL
-    $remoteUrl = git config --get remote.origin.url
 
-    # GitHub
-    if ($remoteUrl -match 'github.com') {
-        $script:ghCmdInfo = Get-Command gh -ErrorAction Ignore
-        if ($null -ne $script:ghCmdInfo) {
-            $listAllPrsCmdJson = Invoke-Expression "gh pr list --json id,author,title,number"
-            $objs = $listAllPrsCmdJson | ConvertFrom-Json | ForEach-Object {
-                [PSCustomObject]@{
-                    PR      = "$($PSStyle.Foreground.Green)" + $_.number
-                    Title   = "$($PSStyle.Foreground.Magenta)" + $_.title
-                    Creator = "$($PSStyle.Foreground.Yellow)" + $_.author.login
+    $filterCurrentUser = $true
+    $reloadPrList = $false
+
+    # loop due to requesting possibly selecting current user
+    do
+    {
+        # find the repo remote URL
+        $remoteUrl = git config --get remote.origin.url
+
+        # GitHub
+        if ($remoteUrl -match 'github.com') {
+            $script:ghCmdInfo = Get-Command gh -ErrorAction Ignore
+            if ($null -ne $script:ghCmdInfo) {
+                if ($filterCurrentUser) {
+                    $currentUser = Invoke-Expression "gh api user --jq '.login'"
+                    $listAllPrsCmdJson = Invoke-Expression "gh pr list --json id,author,title,number --author $currentUser"
+                } else {
+                    $currentUser = $null
+                    $listAllPrsCmdJson = Invoke-Expression "gh pr list --json id,author,title,number"
+                }
+
+                $objs = $listAllPrsCmdJson | ConvertFrom-Json | ForEach-Object {
+                    [PSCustomObject]@{
+                        PR      = "$($PSStyle.Foreground.Green)" + $_.number
+                        Title   = "$($PSStyle.Foreground.Magenta)" + $_.title
+                        Creator = "$($PSStyle.Foreground.Yellow)" + $_.author.login
+                    }
+                }
+            }
+            else {
+                Write-Error "Repo is a GitHub repo and gh command not found"
+                return
+            }
+            $webCmd = 'gh pr view {1} --web'
+            $previewCmd = 'gh pr view {1} && gh pr diff {1}'
+        }
+        # Azure DevOps
+        elseif ($remoteUrl -match 'dev.azure.com|visualstudio.com') {
+            $script:azCmdInfo = Get-Command az -ErrorAction Ignore
+            if ($null -ne $script:azCmdInfo) {
+                if ($filterCurrentUser) {
+                    $currentUser = Invoke-Expression "az account show --query user.name --output tsv"
+                    $listAllPrsCmdJson = Invoke-Expression $('az repos pr list --status "active" --query "[].{title: title, number: pullRequestId, creator: createdBy.uniqueName}"' + "--creator $currentUser")
+                } else {
+                    $currentUser = $null
+                    $listAllPrsCmdJson = Invoke-Expression 'az repos pr list --status "active" --query "[].{title: title, number: pullRequestId, creator: createdBy.uniqueName}"'
+                }
+
+                $objs = $listAllPrsCmdJson | ConvertFrom-Json | ForEach-Object {
+                    [PSCustomObject]@{
+                        PR      = "$($PSStyle.Foreground.Green)" + $_.number
+                        Title   = "$($PSStyle.Foreground.Magenta)" + $_.title
+                        Creator = "$($PSStyle.Foreground.Yellow)" + $_.creator
+                    }
+                }
+            }
+            else {
+                Write-Error "Repo is an Azure DevOps repo and az command not found"
+                return
+            }
+            $webCmd = 'az repos pr show --id {1} --open --output none'
+            # currently errors on query. Need to fix instead of output everything
+            #$previewCmd = 'az repos pr show --id {1} --query "{Created:creationDate, Closed:closedDate, Creator:createdBy.displayName, PR:codeReviewId, Title:title, Repo:repository.name, Reviewers:join('', '',reviewers[].displayName), Source:sourceRefName, Target:targetRefName}" --output yamlc'
+            $previewCmd = 'az repos pr show --id {1} --output yamlc'
+        }
+
+        $fzfArguments = Get-GitFzfArguments
+        $fzfArguments['Bind'] += 'ctrl-o:execute-silent(' + $webCmd + ')'
+        $header = "CTRL-O (open in browser) / CTRL-X (checks) / CTRL+U (toggle user filter)`n`n"
+
+        $prevCLICOLOR_FORCE = $env:CLICOLOR_FORCE
+        $prevOutputRendering = $PSStyle.OutputRendering
+
+        $env:CLICOLOR_FORCE = 1 # make gh show keep colors
+        $PSStyle.OutputRendering = 'Ansi'
+
+        try {
+            $borderLabel = "🆕 Pull Requests"
+            if ($currentUser) {
+                $borderLabel += " by $currentUser"
+            }
+            $result = $objs | out-string -Stream  | `
+                Where-Object { -not [string]::IsNullOrWhiteSpace($_) } | `
+                Invoke-Fzf @fzfArguments -Expect "ctrl-x,ctrl-u" -Header $header -Preview "$previewCmd" -HeaderLines 2 -BorderLabel $borderLabel
+
+            $prId = $result.Split(' ')[1] # get the PR ID
+            $reloadPrList = $result[0] -eq 'ctrl-u' # reload if user filter toggled
+            $checks = $null
+            if ($reloadPrList) {
+                $filterCurrentUser = -not $filterCurrentUser
+            }
+            elseif ($result[0] -eq 'ctrl-x') {
+                if ($remoteUrl -match 'github.com') {
+                    $env:CLICOLOR_FORCE = $prevCLICOLOR_FORCE
+                    $checksCmd = "gh pr view $prId --json ""statusCheckRollup"""
+                    $checksJsonTxt = Invoke-Expression $checksCmd
+                    $checksJson = $checksJsonTxt | ConvertFrom-Json
+                    $checks = $checksJson.statusCheckRollup | ForEach-Object {
+                        if ($_.status -eq 'COMPLETED') {
+                            if ($_.conclusion -eq 'SUCCESS') {
+                                $status = "$($PSStyle.Foreground.Green)" + "✅ Success"
+                            }
+                            else {
+                                $status = "$($PSStyle.Foreground.Red)" + "⛔ Failed"
+                            }
+                        }
+                        else {
+                            $status = "$($PSStyle.Foreground.Yellow)" + "⏳"
+                        }
+                        [PSCustomObject]@{
+                            Status = $status
+                            Check  = "$($PSStyle.Foreground.Magenta)" + $_.name
+                            Link   = $_.detailsUrl
+                        }
+                    }
+                    #$runCheckCmd = $null
+                    $runCheckCmd = 'echo running '
+                }
+                elseif ($remoteUrl -match 'dev.azure.com|visualstudio.com') {
+                    $checksCmd = "az repos pr policy list --id $prId --output json"
+                    $checksJsonTxt = Invoke-Expression $checksCmd
+                    $checksJson = $checksJsonTxt | ConvertFrom-Json
+
+                    # only worried about blocking checks, for now:
+                    $checks = $checksJson | Where-Object { $_.configuration.isBlocking } | ForEach-Object {
+                        $context = $_.context
+                $settings = $_.configuration.settings
+                    $type = $_.configuration.type
+                        $link = $remoteUrl,"pullrequest/$($prId)" -join '/' # default to opening PR in browser
+
+                # find check status:
+                        switch ($_.status) {
+                            'approved' {
+                                $status = "$($PSStyle.Foreground.Green)" + "✅ Approved"
+                            }
+                            'rejected' {
+                                $status = "$($PSStyle.Foreground.Red)" + "⛔ Rejected"
+                            }
+                            'queued' {
+                                if ($context -and $context.IsExpired) {
+                                    $status = "$($PSStyle.Foreground.Red)" + "⌚ Expired"
+                                }
+                                else {
+                                    $status = "$($PSStyle.Foreground.BrightBlue)" + "🔵 Queued"
+                                }
+                            }
+                'running' {
+                    $status = "$($PSStyle.Foreground.BrightBlue)" + "⏳ Running"
+                }
+                            default {
+                                $status =  $_.status # unknown status
+                            }
+                        }
+
+                # find check name and build link:
+                switch ($type.displayName) {
+                'Build' {
+                    $check = $settings.displayName
+                    if ($context) {
+                        $buildId = $context.buildId
+                                    $link = $remoteUrl.split('/_git/')[0],"_build/results?buildId=$buildId" -join '/'
+                    }
+                }
+                'Status' {
+                    $check = $settings.defaultDisplayName
+                }
+                default {
+                    $check = $type.displayName
+                }
+                }
+                        [PSCustomObject]@{
+                            EvaluationId = "$($PSStyle.Foreground.Blue)" + $_.evaluationId
+                            Status = $status
+                            Check  = "$($PSStyle.Foreground.Magenta)" + $check
+                            Link   = $link
+                        }
+                    }
+
+                    $runCheckCmd = "az repos pr policy queue --id $prId --output none --evaluation-id "
+                }
+            }
+
+            # 2. Run the checks command, if selected in previous command:
+            if ($null -ne $checks) {
+                $fzfArguments = Get-GitFzfArguments
+                #$fzfArguments['Bind'] += 'ctrl-r:execute(' + $runCheckCmd + ')'
+                if ($runCheckCmd) {
+                    $fzfArguments['Expect'] = "ctrl-r"
+                    $header = "CTRL-R (run selected check)`n`n"
+                } else {
+                    $header = "`n"
+                }
+                $env:CLICOLOR_FORCE = 1 # make gh show keep colors
+                $PSStyle.OutputRendering = 'Ansi'
+
+                $result = $checks | out-string -Stream  | `
+                    Where-Object { -not [string]::IsNullOrWhiteSpace($_) } | `
+                    Invoke-Fzf @fzfArguments -Header $header -HeaderLines 2 -BorderLabel '✅ Checks'
+
+                if ($runCheckCmd -and $result[0] -eq 'ctrl-r') {
+                    $cmd = $($runCheckCmd + $($result[1] -split ' ')[0])
+                    $cmd
+                    Write-Warning "Running check using command '$cmd'..."
+                    Invoke-Expression $cmd
                 }
             }
         }
-        else {
-            Write-Error "Repo is a GitHub repo and gh command not found"
-            return
+        finally {
+            $env:CLICOLOR_FORCE = $prevCLICOLOR_FORCE
+            $PSStyle.OutputRendering = $prevOutputRendering
         }
-        $webCmd = 'gh pr view {1} --web'
-        $previewCmd = 'gh pr view {1} && gh pr diff {1}'
-    }
-    # Azure DevOps
-    elseif ($remoteUrl -match 'dev.azure.com|visualstudio.com') {
-        $script:azCmdInfo = Get-Command az -ErrorAction Ignore
-        if ($null -ne $script:azCmdInfo) {
-            $listAllPrsCmdJson = Invoke-Expression 'az repos pr list --status "active" --query "[].{title: title, number: pullRequestId, creator: createdBy.uniqueName}"'
-            $objs = $listAllPrsCmdJson | ConvertFrom-Json | ForEach-Object {
-                [PSCustomObject]@{
-                    PR      = "$($PSStyle.Foreground.Green)" + $_.number
-                    Title   = "$($PSStyle.Foreground.Magenta)" + $_.title
-                    Creator = "$($PSStyle.Foreground.Yellow)" + $_.creator
-                }
-            }
-        }
-        else {
-            Write-Error "Repo is an Azure DevOps repo and az command not found"
-            return
-        }
-        $webCmd = 'az repos pr show --id {1} --open --output none'
-        # currently errors on query. Need to fix instead of output everything
-        #$previewCmd = 'az repos pr show --id {1} --query "{Created:creationDate, Closed:closedDate, Creator:createdBy.displayName, PR:codeReviewId, Title:title, Repo:repository.name, Reviewers:join('', '',reviewers[].displayName), Source:sourceRefName, Target:targetRefName}" --output yamlc'
-        $previewCmd = 'az repos pr show --id {1} --output yamlc'
-    }
+    } while ($reloadPrList)
 
-    $fzfArguments = Get-GitFzfArguments
-    $fzfArguments['Bind'] += 'ctrl-o:execute-silent(' + $webCmd + ')'
-    $header = "CTRL-O (open in browser)`n`n"
-
-    $prevCLICOLOR_FORCE = $env:CLICOLOR_FORCE
-    $prevOutputRendering = $PSStyle.OutputRendering
-
-    $env:CLICOLOR_FORCE = 1 # make gh show keep colors
-    $PSStyle.OutputRendering = 'Ansi'
-
-    try {
-        $result = @()
-        $objs | out-string -Stream  | Where-Object { -not [string]::IsNullOrWhiteSpace($_) } | `
-            Invoke-Fzf @fzfArguments -Header $header -Preview "$previewCmd" -HeaderLines 2 -BorderLabel '🆕 Pull Requests' | `
-            ForEach-Object {
-            $result += $_.Split(' ')[0] # get the PR ID
-        }
-    }
-    finally {
-        $env:CLICOLOR_FORCE = $prevCLICOLOR_FORCE
-        $PSStyle.OutputRendering = $prevOutputRendering
-    }
-
-    $result
+    $prId
 }
