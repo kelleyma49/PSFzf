5548f8fa9676d6059289e9be99f241cb943f5400
Klaus Jaschan <klaus.jaschan@gmail.com>
2025-03-05 22:13:31 -0300

    change of bat language from markdown to man for command help

--- helpers/PsFzfTabExpansion-Preview.ps1
+++ helpers/PsFzfTabExpansion-Preview.ps1
@@ -46,7 +46,7 @@ elseif (($cmdResults = Get-Command $Item -ErrorAction Ignore)) {
     if ($cmdResults) {
         if ($cmdResults.CommandType -ne 'Application') {
             if ($ansiCompatible -and $(Get-Command bat -ErrorAction Ignore)) {
-                Get-Help $Item | bat --language=markdown --color always --style=plain
+                Get-Help $Item | bat --language=man --color always --style=plain
             }
             else {
 
